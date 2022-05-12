b897290f90d8b042377d2ef1d1009e6d1b49bbe9
Alejandro Melo Meza <ale.melo.meza@icloud.com>
2022-05-12 15:53:41 -0400

    first commit

---
+++ poc-uom/app/application/application.go
@@ -0,0 +1,96 @@
+package application
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	log = logrus.New()
+)
+
+var (
+	counter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "myapp_processed_ops_total",
+		Help: "The total number of processed events",
+	})
+	histogram = promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Name:    "myapp_greeting_seconds",
+		Help:    "Time take to greet someone",
+		Buckets: []float64{1, 2, 5, 6, 10},
+	}, []string{"code"})
+)
+
+func StartApp() {
+	log.SetFormatter(&logrus.JSONFormatter{
+		FieldMap: logrus.FieldMap{
+			logrus.FieldKeyTime: "timestamp",
+			logrus.FieldKeyMsg:  "message",
+		},
+	})
+	log.WithFields(logrus.Fields{
+		"payload": "myPayload",
+	}).Info("myMessage")
+
+	e := echo.New()
+	e.HideBanner = true
+	e.Debug = false
+	e.Use(middleware.CORS())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "start...")
+	})
+	e.GET("/counter", func(c echo.Context) error {
+		counter.Inc()
+		return c.String(http.StatusOK, "counter")
+	})
+	e.GET("/histogram", func(c echo.Context) error {
+		start := time.Now()
+		rand.Seed(time.Now().UnixNano())
+		n := rand.Intn(12)
+		time.Sleep(time.Duration(n) * time.Second)
+		httpDuration := time.Since(start)
+		histogram.WithLabelValues(fmt.Sprintf("%d", http.StatusOK)).Observe(httpDuration.Seconds())
+		return c.String(http.StatusOK, "histogram")
+	})
+	e.GET("/metrics", func(c echo.Context) error {
+		h := promhttp.Handler()
+		h.ServeHTTP(c.Response(), c.Request())
+		return nil
+	})
+
+	s := &http.Server{
+		Addr:         ":1313",
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	go func() {
+		if err := e.StartServer(s); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ poc-uom/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/alemelomeza/poc-golden-signals/app/application"
+
+func main() {
+	application.StartApp()
+}
